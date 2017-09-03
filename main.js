bb8283f3fc7217be6e8c0dcf1ede6864b513f8a1
Steven <stevenez95@gmail.com>
2017-09-02 23:11:35 -0600

    updater

--- main.js
+++ main.js
@@ -1,7 +1,63 @@
 // ./main.js
 const {app, BrowserWindow} = require('electron')
+const path = require('path');
+const url = require('url');
+const updater = require("electron-updater");
+const autoUpdater = updater.autoUpdater;
+
+require('dotenv').config();
 
 let win = null;
+
+///////////////////
+// Auto upadater //
+///////////////////
+autoUpdater.requestHeaders = { "PRIVATE-TOKEN": "Personal access Token" };
+autoUpdater.autoDownload = true;
+
+autoUpdater.setFeedURL({
+    provider: "generic",
+    url: "https://gitlab.com/_example_repo_/-/jobs/artifacts/master/raw/dist?job=build"
+});
+
+autoUpdater.on('checking-for-update', function () {
+    sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', function (info) {
+    sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', function (info) {
+    sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', function (err) {
+    sendStatusToWindow('Error in auto-updater.');
+});
+
+autoUpdater.on('download-progress', function (progressObj) {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+    sendStatusToWindow('Update downloaded; will install in 1 seconds');
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+    setTimeout(function () {
+        autoUpdater.quitAndInstall();
+    }, 1000);
+});
+
+autoUpdater.checkForUpdates();
+
+function sendStatusToWindow(message) {
+    console.log(message);
+}
 
 app.on('ready', function () {
 
@@ -9,7 +65,16 @@ app.on('ready', function () {
   win = new BrowserWindow({width: 1000, height: 600});
 
   // Specify entry point
-  win.loadURL('http://localhost:4200');
+  if (process.env.PACKAGE === 'true'){
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'dist/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  } else {
+    win.loadURL(process.env.HOST);
+    win.webContents.openDevTools();
+  }
 
   // Show dev tools
   // Remove this line before distributing
